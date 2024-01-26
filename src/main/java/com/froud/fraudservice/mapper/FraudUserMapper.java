7ec21e6a8deeee3089f69635cb9c797cf325b560
Igor Simakov <i.simakov@banki.ru>
2024-01-26 12:31:36 +0800

    added: client for open api example

--- src/main/java/com/froud/fraudservice/mapper/FraudUserMapper.java
+++ src/main/java/com/froud/fraudservice/mapper/FraudUserMapper.java
@@ -2,12 +2,21 @@ package com.froud.fraudservice.mapper;
 
 import com.froud.fraudservice.entity.FraudUserEntity;
 import com.froud.fraudservice.server.dto.FraudUser;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(config = MapperConfiguration.class)
 public interface FraudUserMapper {
 
+    @BeanMapping(ignoreByDefault = true)
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "firstName", source = "name")
+    @Mapping(target = "userEmail", source = "email")
     FraudUser toFraudUser(FraudUserEntity fraudUserEntity);
 
+    @BeanMapping(ignoreByDefault = true)
+    @Mapping(target = "name", source = "firstName")
+    @Mapping(target = "email", source = "userEmail")
     FraudUserEntity toFraudUserEntity(FraudUser fraudUser);
 }

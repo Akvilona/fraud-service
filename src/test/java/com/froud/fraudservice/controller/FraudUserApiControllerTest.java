2d073d658fefb6264655eed18d3d3ec0f89c9111
Andrey Antonov <akvilona@list.ru>
2024-02-01 15:20:20 +0300

    Error no

--- src/test/java/com/froud/fraudservice/controller/FraudUserApiControllerTest.java
+++ src/test/java/com/froud/fraudservice/controller/FraudUserApiControllerTest.java
@@ -1,6 +1,7 @@
 package com.froud.fraudservice.controller;
 
 import com.froud.fraudservice.entity.FraudUserEntity;
+import com.froud.fraudservice.server.dto.FraudUser;
 import com.froud.fraudservice.support.IntegrationTestBase;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.ResponseEntity;
@@ -26,19 +27,90 @@ class FraudUserApiControllerTest extends IntegrationTestBase {
     }
 
     @Test
-    void checkFraudUserByEmail2() {
+    void checkFraudUserByEmailFromApi() {
         //GIVEN
         userRepository.save(FraudUserEntity.builder()
             .email("email")
+            .name("name")
             .build());
 
         //WHEN
-        ResponseEntity<Boolean> responseEntity = fraudUserApiControllerUnderTest._checkFraudUserByEmail("email");
+        Boolean responseEntity = getFraudUserByEmail("email", 200);
 
         //THEN
-        assertThat(responseEntity.getBody())
-            .isTrue();
+        assertThat(responseEntity).isTrue();
 
     }
 
+    @Test
+    void deleteFraudUserByIdFromApi() {
+        //GIVEN
+        userRepository.save(FraudUserEntity.builder()
+            .email("email")
+            .name("name")
+            .build());
+
+        // WHEN
+        deleteFraudUserByEmail("1", 200);
+
+        // THEN
+        assertThat(userRepository.findAll()).isEmpty();
+    }
+
+    @Test
+    void postFraudUserById() {
+        //GIVEN
+        FraudUser fraudUserEntity = FraudUser.builder()
+            .id(1L)
+            .firstName("name")
+            .userEmail("emal")
+            .age(18)
+            .build();
+
+        FraudUser fraudUser = postFraudUserByEmail(fraudUserEntity, 200);
+
+        assertThat(fraudUser)
+            .isEqualTo(FraudUser.builder()
+                .id(fraudUserEntity.getId())
+                .firstName(fraudUserEntity.getFirstName())
+                .userEmail(fraudUserEntity.getUserEmail())
+                .build());
+    }
+
+    private FraudUser postFraudUserByEmail(final FraudUser fraudUser, final int status) {
+        return webTestClient.post()
+            .uri(uriBuilder -> uriBuilder
+                .pathSegment("users")
+                .build())
+            .bodyValue(fraudUser)
+            .exchange()
+            .expectStatus().isEqualTo(status)
+            .expectBody(FraudUser.class)
+            .returnResult()
+            .getResponseBody();
+    }
+
+    private void deleteFraudUserByEmail(final String id, final int status) {
+        webTestClient.delete()
+            .uri(uriBuilder -> uriBuilder
+                .pathSegment("users", id)
+                .build())
+            .exchange()
+            .expectStatus().isEqualTo(status)
+            .expectBody()
+            .returnResult();
+    }
+
+    private Boolean getFraudUserByEmail(final String email, final int status) {
+        return webTestClient.get()
+            .uri(uriBuilder -> uriBuilder
+                .pathSegment("users")
+                .queryParam("email", email)
+                .build())
+            .exchange()
+            .expectStatus().isEqualTo(status)
+            .expectBody(Boolean.class)
+            .returnResult()
+            .getResponseBody();
+    }
 }
--- src/test/java/com/froud/fraudservice/support/IntegrationTestBase.java
+++ src/test/java/com/froud/fraudservice/support/IntegrationTestBase.java
@@ -4,11 +4,15 @@ import com.froud.fraudservice.controller.FraudUserApiController;
 import com.froud.fraudservice.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 import java.util.Set;
 
 @AutoConfigureWebTestClient(timeout = "360000")
 public class IntegrationTestBase extends DatabaseAwareTestBase {
+    @Autowired
+    protected WebTestClient webTestClient;
+
     @Autowired
     protected FraudUserApiController fraudUserApiControllerUnderTest;
     @Autowired
